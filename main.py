dc9c302307006080d6a0a27683ad35ae0e1e51a4
brendenwelch <aloofd@gmail.com>
2025-05-15 05:09:29 -0700

    limited framerate to 60

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# ruff: noqa
 import pygame as pg
 from constants import *
 
@@ -5,8 +6,10 @@ from constants import *
 def main():
     # init
     pg.init()
+    clock = pg.time.Clock()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    dt = 0
     while True:
         # input
         for event in pg.event.get():
@@ -20,7 +23,10 @@ def main():
 
         pg.display.flip()
 
+        dt = clock.tick(60) / 1000
+
     # quit
+    pg.quit()
 
 
 if __name__ == "__main__":
