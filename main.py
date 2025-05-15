2d01bdb17fb9b9c6ef8be6a468278c0077ea8630
brendenwelch <aloofd@gmail.com>
2025-05-15 07:02:52 -0700

    a lot. forgot to commit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # ruff: noqa
+import sys
 import pygame as pg
+import player as pl
+import shot as sh
+import asteroid as ast
+import asteroidfield as fld
 from constants import *
 
 
@@ -9,6 +14,19 @@ def main():
     clock = pg.time.Clock()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pg.sprite.Group()
+    drawable = pg.sprite.Group()
+    asteroids = pg.sprite.Group()
+    shots = pg.sprite.Group()
+
+    pl.Player.containers = (updatable, drawable)
+    ast.Asteroid.containers = (asteroids, updatable, drawable)
+    fld.AsteroidField.containers = updatable
+    sh.Shot.containers = (shots, updatable, drawable)
+
+    player = pl.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = fld.AsteroidField()
+
     dt = 0
     while True:
         # input
@@ -17,9 +35,20 @@ def main():
                 return
 
         # logic
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.colliding(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if shot.colliding(asteroid):
+                    shot.kill()
+                    asteroid.kill()
 
         # render
         screen.fill(pg.Color(0, 0, 0))
+        for item in drawable:
+            item.draw(screen)
 
         pg.display.flip()
 
---
+++ player.py
@@ -0,0 +1,56 @@
+import pygame as pg
+import circleshape
+import shot as sh
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_SHOOT_COOLDOWN,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+    PLAYER_SHOOT_SPEED,
+)
+
+
+class Player(circleshape.CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        self.shot_delay = 0
+
+    def triangle(self):
+        forward = pg.Vector2(0, 1).rotate(self.rotation)
+        right = pg.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pg.draw.polygon(screen, pg.Color(255, 255, 255), self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pg.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        if self.shot_delay <= 0:
+            self.shot_delay = PLAYER_SHOOT_COOLDOWN
+            shot = sh.Shot(self.position.x, self.position.y)
+            shot.velocity = pg.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
+    def update(self, dt):
+        self.shot_delay -= dt
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_a]:
+            self.rotate(-dt)
+        if keys[pg.K_d]:
+            self.rotate(dt)
+        if keys[pg.K_w]:
+            self.move(dt)
+        if keys[pg.K_s]:
+            self.move(-dt)
+        if keys[pg.K_SPACE]:
+            self.shoot()
